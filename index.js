39909d762b5bd70f322d355143f97e8a9bd2a252
naims6 <naimdrive6@gmail.com>
2025-11-10 14:57:36 +0600

    project setuped

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = 3000;
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.wsfcvqt.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    const database = client.db("the_book_haven");
+    const allBooksCollection = database.collection("allBooks");
+
+    // latest book api
+    app.get("/latest-books", async (req, res) => {
+      const cursor = allBooksCollection.find().sort({ createdAt: -1 }).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log("Server is running port : ", port);
+});
---
+++ test.js
@@ -0,0 +1,12 @@
+[
+  {
+    title: "Book Title",
+    author: "Author Name",
+    genre: "Fantasy / Mystery / Non-Fiction etc.",
+    rating: "1–5 scale",
+    summary: "Short description of the book",
+    coverImage: "book cover image",
+    userEmail: "email of the user who added the book",
+    createdAt: "created date and time",
+  },
+];
